92609163d00139da85dda9b7a9196de8bd12766e
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-05 13:31:09 +0530

    Update PrimeFactor.cpp

--- PrimeFactor.cpp
+++ PrimeFactor.cpp
@@ -6,9 +6,11 @@ int main()
   scanf("%d",&number);
   for(int i=2;i<=number;i++)
   {
-    if(number%i==0){ prime_factors.push_back(i);}
-  } cout<<"Factors are : "; //printf("Factors of %d are : ",number)
+    if(number%i==0)
+    prime_factors.push_back(i);
+  } 
+   cout<<"Factors are : "; //printf("Factors of %d are : ",number)
    for (auto display : prime_factors)
-    cout<<display<<' ';
+   cout<<display<<' ';
   return 0;
 }
