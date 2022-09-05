0200fdf602570f6edf7e3aa4364dd09abb083468
mckinsey <92459398+mckinsy@users.noreply.github.com>
2022-09-05 14:20:03 +0530

    Update PrimeFactor.cpp

--- PrimeFactor.cpp
+++ PrimeFactor.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main()
 {
   vector<int> prime_factors; int number;
-  scanf("%d",&number);
+  scanf("%d",&number);  // latest compiler works even if  ' & '  not placed before variable in scanf and if by mistake  ' & ' is placed before variable in printf
   for(int i=2;i<=number;i++)
   {
     if(number%i==0)
@@ -15,7 +15,7 @@ int main()
    {  cout<<" and "<<prime_factors[0]<<" only";  return 0;  }
    else
    {
-   for (auto display : prime_factors)
+   for (auto display : prime_factors) // also   for(auto &display : prime_factors)  works well even  ' & '  is not used
    {
      if(display==last2nd_element)
      cout<<" and ";
